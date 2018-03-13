2ca8cd4a9640f8f79a16e881cebfe562bea91ad7
KRITIK BHIMANI <bhimanikritik@gmail.com>
2018-03-13 13:59:40 +0530

    fix test for imul and add library dependencies for chisel-iotesters

--- build.sbt
+++ build.sbt
@@ -6,4 +6,9 @@ name := "boom"
 
 scalaVersion := "2.11.6"
 
+
 addCompilerPlugin("org.scalamacros" % "paradise_2.11.8" % "2.1.0")
+
+libraryDependencies ++= Seq("org.scala-lang" % "scala-reflect" % scalaVersion.value) ++
+			Seq("edu.berkeley.cs" %% "chisel-iotesters" % "latest.release")
+
--- src/test/scala/IMulUnitTest.scala
+++ src/test/scala/IMulUnitTest.scala
@@ -27,7 +27,7 @@ class IMulUnitTester(c: IMul) extends PeekPokeTester(c) {
 }
 
 // Invoke test with:
-//    $ sbt 'test-only boom.IMulSpec'
+//    $ sbt 'testOnly boom.IMulSpec'
 //
 class IMulSpec extends FlatSpec with Matchers {
    behavior of "IMulSpec"
@@ -37,7 +37,7 @@ class IMulSpec extends FlatSpec with Matchers {
    }
 
    it should "compute imul excellently" in {
-      chisel3.iotesters.Driver(() => new IMul(imul_stages = 3)) { c =>
+      chisel3.iotesters.Driver.execute(() => new IMul(imul_stages = 3),manager) { c =>
          new IMulUnitTester(c)
       } should be(true)
    }
@@ -74,7 +74,7 @@ class IMulHWIOUnitTester extends SteppedHWIOTester {
 }
 
 // Invoke test with:
-//    $ sbt 'test-only boom.IMulTester'
+//    $ sbt 'testOnly boom.IMulTester'
 //
 class IMulTester extends ChiselFlatSpec {
   "IMul" should "compile and run without incident" in {
